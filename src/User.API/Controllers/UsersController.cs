59a7209f9ef28006b9cc0de9c94d301aa186f8c8
limengjie <li.mengjie@yalla.live>
2019-12-02 15:09:42 +0800

    add  json patch

--- src/User.API/Controllers/UsersController.cs
+++ src/User.API/Controllers/UsersController.cs
@@ -1,9 +1,12 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using User.API.Data;
+using User.API.Entity.Models;
 using User.API.Filters;
 
 namespace User.API.Controllers
@@ -24,13 +27,56 @@ namespace User.API.Controllers
         [HttpGet]
         public async Task<ActionResult> Get()
         {
-            var user = _useContext.Users
+            var user = await _useContext.Users
                 .AsNoTracking()
-                .Include(x => x.Properties)
-                .SingleOrDefault(x => x.Id == UserIdentity.UserId);
+                .Include(p => p.Properties)
+                .SingleOrDefaultAsync(u => u.Id == UserIdentity.UserId);
+
+            if (user == null)
+                //return NotFound();
+                throw new UserOperationException($"错误的用户上下文Id={UserIdentity.UserId}");
+
+            return Ok(user);
+        }
+
+        [Route("")]
+        [HttpPatch]
+        public async Task<ActionResult> Patch([FromBody]JsonPatchDocument<AppUser> patch)
+        {
+            var user = await _useContext.Users
+                .SingleOrDefaultAsync(u => u.Id == UserIdentity.UserId);
 
             if (user == null)
                 throw new UserOperationException($"错误的用户上下文Id={UserIdentity.UserId}");
+
+            patch.ApplyTo(user);
+
+            var userProperties = user.Properties == null ? new List<UserProperty>() : user.Properties;
+            foreach (var property in userProperties)
+            {
+                _useContext.Entry(property).State = EntityState.Detached;
+            }
+
+            var originProperties = await _useContext.UserProperties
+                .AsNoTracking()
+                .Where(u => u.AppUserId == UserIdentity.UserId).ToListAsync();
+            var allProperties = originProperties.Union(userProperties).Distinct();
+
+            var removeProperties = originProperties.Except(userProperties);
+            var newProperties = allProperties.Except(originProperties);
+
+            foreach (var property in removeProperties)
+            {
+                _useContext.Remove(property);
+            }
+
+            foreach (var property in newProperties)
+            {
+                _useContext.Add(property);
+            }
+
+            _useContext.Users.Update(user);
+            _useContext.SaveChanges();
 
             return Ok(user);
         }
--- src/User.API/Entity/Models/UserProperty.cs
+++ src/User.API/Entity/Models/UserProperty.cs
@@ -11,5 +11,53 @@ namespace User.API.Entity.Models
         public string Text { get; set; }
 
         public string Value { get; set; }
+
+        int? _requestedHashCode;
+
+        public override int GetHashCode()
+        {
+            if (!IsTransient())
+            {
+                if (!_requestedHashCode.HasValue)
+                    //xor for random
+                    _requestedHashCode = (this.Key + this.Value).GetHashCode() ^ 31;
+                return _requestedHashCode.Value;
+            }
+            else
+                return base.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is UserProperty))
+                return false;
+
+            if (object.ReferenceEquals(this, obj))
+                return true;
+
+            UserProperty item = (UserProperty)obj;
+            if (item.IsTransient() || this.IsTransient())
+                return false;
+            else
+                return item.Key == this.Key && item.Value == this.Value;
+        }
+
+        public bool IsTransient()
+        {
+            return string.IsNullOrEmpty(this.Key) || string.IsNullOrEmpty(this.Value);
+        }
+
+        public static bool operator ==(UserProperty left, UserProperty right)
+        {
+            if (object.Equals(left, null))
+                return (object.Equals(right, null)) ? true : false;
+            else
+                return left.Equals(right);
+        }
+
+        public static bool operator !=(UserProperty left, UserProperty right)
+        {
+            return !(left == right);
+        }
     }
 }
