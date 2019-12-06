ea2b31ea442343c68a31a306f0dde5ce46ceea00
limengjie <li.mengjie@yalla.live>
2019-12-06 17:36:58 +0800

    add DataContextFactory

--- src/User.API/Data/UserContext.cs
+++ src/User.API/Data/UserContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using User.API.Entity.Models;
 
 namespace User.API.Data
@@ -39,4 +40,18 @@ namespace User.API.Data
         public DbSet<UserProperty> UserProperties { get; set; }
         public DbSet<UserTage> UserTages { get; set; }
     }
+
+    ///// <summary>
+    ///// 为EF的Migration准备的辅助类，填写完整连接字符串和数据库类型
+    ///// 就可以使用Add-Migration和Update-Database了
+    ///// </summary>
+    //public class DataContextFactory : IDesignTimeDbContextFactory<UserContext>
+    //{
+    //    public UserContext CreateDbContext(string[] args)
+    //    {
+    //        var builder = new DbContextOptionsBuilder<UserContext>();
+    //        builder.UseMySql("server=127.0.0.1;port=3306;database=user_api;uid=root;pwd=password;IgnoreCommandTransaction=true;");
+    //        return new UserContext(builder.Options);
+    //    }
+    //}
 }
